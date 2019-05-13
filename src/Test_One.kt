3e11cbd4f52fd3606a66356d1ad147c8b102dd99
rbrandon87 <rbrandon87@gmail.com>
2019-05-13 18:32:33 -0400

    Test Commit One

---
+++ src/Test_One.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+
+    val name = "Brandon"
+    var age = 35
+
+    if (age == 18){
+        println("You are an adult!")
+    } else if (age == 21){
+        println("You can drink")
+    }else{
+        println("You are old")
+    }
+
+}
